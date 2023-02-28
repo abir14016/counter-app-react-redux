9d9fa9026e2860eec958429b18432807099ad5e6
abir14016 <abir14016@gmail.com>
2023-03-01 02:33:45 +0600

    custom middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
+//custom middleware
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upComingState = [action].reduce(rootReducer, store.getState());
+    console.log(`Upcoming State: ${JSON.stringify(upComingState)}`)
+
+    //passing action
+    next(action);
+}
+
 //creating store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
